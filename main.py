e88826984fb8b7429404d7a4f63b405912eaba97
Unknown <6bkysrb99@gmail.com>
2017-06-20 14:52:29 +0900

    make code

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sys
 import PySide.QtGui
 import PySide.QtUiTools
+import PySide.QtCore
+import os.path
 
 
 class MainForm(PySide.QtGui.QDialog):
     def __init__(self, parent=None):
         super(MainForm, self).__init__(parent)
         self.ui = PySide.QtUiTools.QUiLoader().load('./MainUI.ui')
+        self.establishConnection()
+
+    def openFile(self):
+    	filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+'/Desktop')
+
+
+    def establishConnection(self):	
+        self.ui.Open_file_button.clicked.connect(self.openFile)
 
 if __name__ == '__main__':
     app = PySide.QtGui.QApplication(sys.argv)
