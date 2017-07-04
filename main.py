86f946c3f8d09b9c367ce9ce23049635aab69e4e
Unknown <6bkysrb99@gmail.com>
2017-07-04 15:53:48 +0900

    make code

--- main.py
+++ main.py
@@ -16,23 +16,38 @@ class MainForm(PySide.QtGui.QMainWindow,PySide.QtGui.QDialog):
         # self.ui.view1.setScene(self.scene)
            
         self.establish_connection()
-        self.button1=PySide.QtGui.QPushButton('open_file')     
+        self.button_1=PySide.QtGui.QPushButton('open_file_1')
+        self.button_2=PySide.QtGui.QPushButton('open_file_2')       
         
-    def open_file(self):
-        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/')
+    def open_file_1(self):
+        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/pictures/')
         self.x=filename[0]
-        self.picture(self.x)
+        self.picture_1(self.x)
+
+    def open_file_2(self):
+        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/pictures/')
+        self.y=filename[0]
+        self.picture_2(self.y)
         
     def establish_connection(self):
-        self.ui.button1.clicked.connect(self.open_file)
+        self.ui.button_1.clicked.connect(self.open_file_1)
+        self.ui.button_2.clicked.connect(self.open_file_2)
 
-    def picture(self,x):
+    def picture_1(self,x):
         pixmap=PySide.QtGui.QPixmap()
         pixmap.load(self.x)
         self.scene=PySide.QtGui.QGraphicsScene(self)
         item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
         self.scene.addItem(item)
-        self.ui.view1.setScene(self.scene)
+        self.ui.view_1.setScene(self.scene)
+
+    def picture_2(self,y):
+        pixmap=PySide.QtGui.QPixmap()
+        pixmap.load(self.y)
+        self.scene=PySide.QtGui.QGraphicsScene(self)
+        item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
+        self.scene.addItem(item)
+        self.ui.view_2.setScene(self.scene)
 
 if __name__=='__main__':
     app=PySide.QtGui.QApplication(sys.argv)
