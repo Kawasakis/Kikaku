c02de7eed752d4e22630bda24f6026889396de76
Unknown <6bkysrb99@gmail.com>
2017-07-04 13:50:43 +0900

    show picture

--- main.py
+++ main.py
@@ -5,23 +5,42 @@ import PySide.QtCore
 import os.path
 
 
-class MainForm(PySide.QtGui.QDialog):
-    def __init__(self, parent=None):
-        super(MainForm, self).__init__(parent)
-        self.ui = PySide.QtUiTools.QUiLoader().load('./MainUI.ui')
-        self.establishConnection()
+class MainForm(PySide.QtGui.QMainWindow,PySide.QtGui.QDialog):
+    def __init__(self,parent=None):
+        super(MainForm,self).__init__(parent)
+        self.ui=PySide.QtUiTools.QUiLoader().load('./MainUI_2.ui')
+                
+        # self.scene=PySide.QtGui.QGraphicsScene(self)
+        # item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
+        # self.scene.addItem(item)
+        # self.ui.view1.setScene(self.scene)
+           
+        self.establish_connection()
+        self.button1=PySide.QtGui.QPushButton('open_file')     
+        
+    def open_file(self):
+        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/')
+        self.x=filename[0]
+        self.picture(self.x)
+        
+    def establish_connection(self):
+        self.ui.button1.clicked.connect(self.open_file)
 
-    def openFile(self):
-    	filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+'/Desktop')
+    def picture(self,x):
+        pixmap=PySide.QtGui.QPixmap()
+        pixmap.load(self.x)
+        self.scene=PySide.QtGui.QGraphicsScene(self)
+        item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
+        self.scene.addItem(item)
+        self.ui.view1.setScene(self.scene)
 
+if __name__=='__main__':
+    app=PySide.QtGui.QApplication(sys.argv)
+    
+    # pixmap=PySide.QtGui.QPixmap()
+    # pixmap.load('')
 
-    def establishConnection(self):	
-        self.ui.Open_file_button.clicked.connect(self.openFile)
-
-if __name__ == '__main__':
-    app = PySide.QtGui.QApplication(sys.argv)
-
-    main_form = MainForm()
+    main_form=MainForm()
     main_form.ui.show()
-
+        
     sys.exit(app.exec_())
