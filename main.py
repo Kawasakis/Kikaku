f1312ee81d101012060b6896600f2bf7d705a4fe
Unknown <6bkysrb99@gmail.com>
2017-07-25 16:30:25 +0900

    make code

--- main.py
+++ main.py
@@ -1,61 +1,57 @@
 import sys
-import PySide.QtGui
-import PySide.QtUiTools
-import PySide.QtCore
+import PyQt4.QtGui
+import PyQt4.uic
+import PyQt4.QtCore
 import os.path
+import random
+import glob
+from PIL import Image
+import numpy as np
 
 
-class MainForm(PySide.QtGui.QMainWindow,PySide.QtGui.QDialog):
-    def __init__(self,parent=None):
+
+class MainForm(PyQt4.QtGui.QMainWindow):
+    def __init__(self,filelist=[],parent=None):
         super(MainForm,self).__init__(parent)
-        self.ui=PySide.QtUiTools.QUiLoader().load('./MainUI_2.ui')
-                
-        # self.scene=PySide.QtGui.QGraphicsScene(self)
-        # item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
-        # self.scene.addItem(item)
-        # self.ui.view1.setScene(self.scene)
-           
+        PyQt4.uic.loadUi('./MainUI_2.ui', self)
+        self.filelist=glob.glob('/home/kanglab/Desktop/kikaku/pictures/*.jpg')                 
         self.establish_connection()
-        self.button_1=PySide.QtGui.QPushButton('open_file_1')
-        self.button_2=PySide.QtGui.QPushButton('open_file_2')       
+    
         
     def open_file_1(self):
-        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/pictures/')
-        self.x=filename[0]
-        self.picture_1(self.x)
+        filename=PyQt4.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/pictures/')
+        self.x=filename
+        self.picture_1()
 
     def open_file_2(self):
-        filename=PySide.QtGui.QFileDialog.getOpenFileName(self,'Open file',os.path.expanduser('~')+ '/Desktop/kikaku/pictures/')
-        self.y=filename[0]
-        self.picture_2(self.y)
+        # filelist=glob.glob('/home/kanglab/Desktop/kikaku/pictures/*.jpg')
+        self.y=self.filelist[random.randint(0,3)]
+        self.picture_2()
         
     def establish_connection(self):
-        self.ui.button_1.clicked.connect(self.open_file_1)
-        self.ui.button_2.clicked.connect(self.open_file_2)
+        self.button_1.clicked.connect(self.open_file_1)
+        self.button_2.clicked.connect(self.open_file_2)
 
-    def picture_1(self,x):
-        pixmap=PySide.QtGui.QPixmap()
-        pixmap.load(self.x)
-        self.scene=PySide.QtGui.QGraphicsScene(self)
-        item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
+    def picture_1(self):      
+        pixmap_1=PyQt4.QtGui.QPixmap(self.x)
+        pixmap_re_1=pixmap_1.scaled(205,255)
+        self.scene=PyQt4.QtGui.QGraphicsScene()
+        item=PyQt4.QtGui.QGraphicsPixmapItem(pixmap_re_1)
         self.scene.addItem(item)
-        self.ui.view_1.setScene(self.scene)
+        self.view_1.setScene(self.scene)
 
-    def picture_2(self,y):
-        pixmap=PySide.QtGui.QPixmap()
-        pixmap.load(self.y)
-        self.scene=PySide.QtGui.QGraphicsScene(self)
-        item=PySide.QtGui.QGraphicsPixmapItem(pixmap)
-        self.scene.addItem(item)
-        self.ui.view_2.setScene(self.scene)
+    def picture_2(self):
+        pixmap_2=PyQt4.QtGui.QPixmap(self.y)
+        pixmap_re_2=pixmap_2.scaled(205,255)
+        self.scene=PyQt4.QtGui.QGraphicsScene()
+        item=PyQt4.QtGui.QGraphicsPixmapItem(pixmap_re_2)
+        self.scene.addItem(item)    
+        self.view_2.setScene(self.scene)
 
 if __name__=='__main__':
-    app=PySide.QtGui.QApplication(sys.argv)
-    
-    # pixmap=PySide.QtGui.QPixmap()
-    # pixmap.load('')
+    app=PyQt4.QtGui.QApplication(sys.argv)
 
     main_form=MainForm()
-    main_form.ui.show()
+    main_form.show()
         
     sys.exit(app.exec_())
